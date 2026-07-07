40ca53882a162b305dd05e5510e9699ce28546c2
thakurhemant879541-cell <thakurhemant879541@gmail.com>
2026-07-07 14:38:22 +0530

    Create script.js

---
+++ frontend/frontend/script.js
@@ -0,0 +1,8 @@
+frontend/style.css
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", () => {
+        alert("🤖 Welcome to Hemant's AI Assistant!");
+    });
+});
